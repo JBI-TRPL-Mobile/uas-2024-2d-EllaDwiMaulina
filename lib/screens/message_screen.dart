dfe303eda3b82e7a602550208a2e0ebf044f1727
EllaDwiMaulina <theyluvwin28@gmail.com>
2025-01-09 18:40:37 +0700

    message._screen

--- lib/screens/message_screen.dart
+++ lib/screens/message_screen.dart
@@ -1,133 +1,87 @@
 import 'package:flutter/material.dart';
-
-class SignUpScreen extends StatelessWidget {
+class MessageScreen  extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 40), // Beri jarak dari atas
-              Text(
-                'Sign up',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 24),
-              // Full Name Field
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Name',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-
-              // Email Field
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Enter Email',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-               TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                  suffixIcon: Icon(Icons.visibility_off), // Icon mata
-                ),
-              ),
-              SizedBox(height: 16),
-
-              // Confirm Password Field
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Confirm',
-                  border: OutlineInputBorder(),
-                  suffixIcon: Icon(Icons.visibility_off), // Icon mata
-                ),
-              ),
-              SizedBox(height: 24),
-
-              // Sign Up Button
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Tambahkan logika untuk Sign Up
-                  },
-                  child: Text('Sign Up'),
-                ),
-              ),
-              SizedBox(height: 16),
-              // "Or sign in with" text
-              Center(
-                child: Text(
-                  'or sign in with',
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ),
-              SizedBox(height: 16),
-              // Social Media Buttons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Google Button
-                  IconButton(
-                    onPressed: () {
-                      // Logika untuk login dengan Google
-                    },
-                    icon: Icon(Icons.g_mobiledata),
-                    iconSize: 32,
-                    color: Colors.red,
-                  ),
-                SizedBox(width: 16),
-                // Facebook Button
-                  IconButton(
-                    onPressed: () {
-                      // Logika untuk login dengan Facebook
-                    },
-                    icon: Icon(Icons.facebook),
-                    iconSize: 32,
-                    color: Colors.blue,
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              // Already have an account
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Don't have an account? "),
-                    GestureDetector(
-                      onTap: () {
-                        // Logika untuk navigasi ke halaman Sign In
-                      },
-                      child: Text(
-                        'Sign Up Now',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-            ],
-                ),
-              ),
-            ],
-          ),
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'Messages',
+          style: TextStyle(color: Colors.black, fontSize: 20),
         ),
-      );
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.black),
+            onPressed: () {
+              // Logika untuk pencarian
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert, color: Colors.black),
+            onPressed: () {
+              // Logika untuk menu lainnya
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: 10, // Jumlah pesan (contoh 10 item)
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Icon(Icons.person, color: Colors.grey), // Placeholder gambar
+            ),
+            title: Text(
+              'Sender Name',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+              'This is a message preview that can be longer...',
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+            onTap: () {
+              // Logika saat item ditekan
+            },
+          );
+        },
+      ),
+       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1, // Set ke "Messages"
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Library',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          // Logika untuk navigasi
+        },
+       ),
+    );
+    
+
+    
   }
 }
